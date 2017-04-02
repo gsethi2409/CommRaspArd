066416f8b3d6ca64d04ed81d78e9f632562bf24e
Gunjan Sethi <gsethi2409@gmail.com>
2017-04-02 18:54:55 +0530

    Add comments

--- RaspPi.py
+++ RaspPi.py
@@ -1,9 +1,9 @@
-import serial
-import time
+import serial   //for Serial communication between the Arduino and Pi.
+import time     //for delay
 while True:
     choice = input("Would you like to change the value of 'n' (y/n)?")
     if choice == 'y':
-        ArduinoPort = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.5")
+        ArduinoPort = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.5")    //open port
         while True:
-            ArduinoPort.write(n);
+            ArduinoPort.write(n);   //send value
     time.sleep(60)
